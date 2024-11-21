e6b4c083596bfb321c8d27d5493d358811f667e9
Arthur Victor <108616332+ArthurLopesVictor@users.noreply.github.com>
2024-11-21 17:18:41 -0300

    Add files via upload

---
+++ scriptCadastro.js
@@ -0,0 +1,11 @@
+function changeFontSize(action){                      //ACTION PARA MOSTRAR A AÇAO QUE A FUNCTION VAI FZR
+    const elements = ['h1','h2','body','p'];        //OQUE VAI AUMENTAR OU DIMINUIR NO SITE
+    elements.map(element=>{                          
+        
+        const selector = document.querySelector(element);                            //MELHORAR O CODIGO PARA NAO USAR DOCUMENTE.QUERYSELECTOR 2 VZS
+        let value = getComputedStyle(selector).getPropertyValue('font-size');       //AUMENTAR A FONTE DE FORMA GRADUAL PARA CADA ELEMENTO
+        value = value.replace('px', '');                                            
+        value = action === 'aumentar' ? parseInt(value) + 1 : parseInt(value) - 1;   //SE FOR AUMENTAR, ELA VAI AUMENTAR 1, CASO NAO, ELA DIMINUI 1
+        selector.style.fontSize =  `${value}px`;                                     
+    })
+}
