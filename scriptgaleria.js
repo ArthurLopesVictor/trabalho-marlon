b5657e3dcbc19410d6cfff2caa83641c22325b37
Arthur Victor <108616332+ArthurLopesVictor@users.noreply.github.com>
2024-11-22 17:41:36 -0300

    Add files via upload

--- scriptgaleria.js
+++ scriptgaleria.js
@@ -9,3 +9,24 @@ function changeFontSize(action){                      //ACTION PARA MOSTRAR A AÃ
         selector.style.fontSize =  `${value}px`;                                     
     })
 }
+
+
+let darkmode = localStorage.getItem('darkmode')
+const themeSwitch = document.getElementById('theme-switch')
+
+const enableDarkmode = () => {
+  document.body.classList.add('darkmode')
+  localStorage.setItem('darkmode', 'active')
+}
+
+const disableDarkmode = () => {
+  document.body.classList.remove('darkmode')
+  localStorage.setItem('darkmode', null)
+}
+
+if(darkmode === "active") enableDarkmode()
+
+themeSwitch.addEventListener("click", () => {
+  darkmode = localStorage.getItem('darkmode')
+  darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+})
