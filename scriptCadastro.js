cec6c223804a1e838c37e9aec60bdedeabd6accf
Arthur Victor <108616332+ArthurLopesVictor@users.noreply.github.com>
2024-11-25 14:59:32 -0300

    Add files via upload

--- scriptCadastro.js
+++ scriptCadastro.js
@@ -30,3 +30,21 @@ themeSwitch.addEventListener("click", () => {
   darkmode = localStorage.getItem('darkmode')
   darkmode !== "active" ? enableDarkmode() : disableDarkmode()
 })
+
+function exibirDataHora() {
+  const agora = new Date();
+  const dataHora = agora.toLocaleString('pt-BR', {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit'
+  });
+  document.getElementById('dataHora').innerText = dataHora;
+}
+
+// Atualiza a data e hora a cada segundo
+setInterval(exibirDataHora, 1000);
+exibirDataHora(); // Chama a função uma vez para exibir imediatamente
